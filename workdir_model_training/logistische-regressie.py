c663f6f489342b24ab15f6678770ffa17bce171f
marcelsiebes-dikw <marcel.siebes@dikw.com>
2022-07-13 12:11:01 +0200

    small updates

--- workdir_model_training/logistische-regressie.py
+++ workdir_model_training/logistische-regressie.py
@@ -5,51 +5,52 @@
 # Library imports
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegressionCV
 import pickle
 
 # Lees de data en sla die op in een pandas dataframe
 df = pd.read_csv("data/BankNote_Authentication.csv")
 
-# Selecteer de predictors en target
-x = df.iloc[:,:-1]
-y = df.iloc[:,-1]
+# Eerst wat elementaire data iinzichten tonen
+print(df.shape)
+print(df.info())
+print(df.describe())
+
+# Bekijk of er sprake is van class imbalance
+print(df['class'].value_counts())
 
 # Maak een scaler object
-std_scaler = StandardScaler()
+scaler = MinMaxScaler() 
 
-# Normaliseer de data
-x_std = pd.DataFrame(std_scaler.fit_transform(x), columns=x.columns)
-x_std
+# Normaliseer de data (door de MinMaxScaler hoeft de kolom 'class' er niet uit.)
+df_norm = scaler.fit_transform(df)
+cols = ['variance', 'skewness', 'kurtosis', 'entropy', 'class']
+df_norm = pd.DataFrame(df_norm, columns=cols)
+df_norm.describe()
 
-# Controleer op missende waardes in x
-print(x_std.isnull().sum())
+# Selecteer de predictors en target
+X = df_norm.iloc[:,:-1]
+y = df_norm.iloc[:,-1]
 
-# Controleer op missende waardes in y
-print(y.isnull().sum())
-
-# Split de data in een training en test set
-x_train, x_test, y_train, y_test = train_test_split(x_std,y, test_size = 0.3, random_state = 0)
+# Split de data in een training en test set:. 70% training en 30% test.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
 # Implementeer Logistische Regressie met ingebouwde cross-validation
 logistische_regressie = LogisticRegressionCV(cv=10)
-logistische_regressie.fit(x_train, y_train)
+logistische_regressie.fit(X_train, y_train)
 
 # Predict de target van de test set
-y_pred = logistische_regressie.predict(x_test)
+y_pred = logistische_regressie.predict(X_test)
 
 # Bepaal de AUC score
 score = roc_auc_score(y_test, y_pred)
-score
-
-# Voorbeeld van een vals bankbiljet
-logistische_regressie.predict([[-2, -3, 1.1, 2.41]])
-
-# Voorbeeld van een goed bankbiljet
-logistische_regressie.predict([[2, 3, 4, 1]])
+print("AUC Score {}", score)
+print(classification_report(y_test, y_pred))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
 
 # Maak een Pickle file (serialization) voor gebruik in de container
 pickle_file = open("logistische-regressie.pkl", "wb")
