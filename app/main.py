24c51274ab819b34f39aaf298951d5c5d804d1ba
MarcelSiebes <57792298+MarcelSiebes@users.noreply.github.com>
2022-07-16 11:17:04 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -54,4 +54,4 @@ def predict_bankbiljet(data:bankbiljet):
 
 # Main
 if __name__ == '__main__':
-    uvicorn.run(app, host = '0.0.0.0', port = 8000)
+    uvicorn.run(app, host = '0.0.0.0', port = 80)
